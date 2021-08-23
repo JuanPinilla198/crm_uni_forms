c10320b52bff3501edf6ff7f810737075ee1ccd6
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-08-23 18:16:23 -0500

    Update crm_lead.py

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -3520,7 +3520,7 @@ class Lead(models.Model):
     )
 
     country_id = fields.Many2one('res.country', "Country")
-    #xcity = fields.Many2one('res.country.state.city', "8. Municipio de Residencia")
+    xcity = fields.Many2one('res.country.state.city', "8. Municipio de Residencia")
     city = fields.Char(related="xcity.name")
     state_id = fields.Many2one('res.country.state', 'State')
     
