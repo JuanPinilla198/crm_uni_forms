5f73272f0f77b2af5b31d31099ffd0e69f124f56
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-08-23 18:13:44 -0500

    Update crm_lead.py

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -15,31 +15,10 @@ class CountryStateCity(models.Model):
     name = fields.Char('City Name', size=64, )
     state_id = fields.Many2one('res.country.state', 'State')
     country_id = fields.Many2one('res.country', 'Country', default='Colombia')
-    xcity = fields.Many2one('res.country.state.city', "8. Municipio de Residencia")
+    
     _order = 'code'
     
-    @api.onchange('country_id', 'state_id')
-    def onchange_location(self):
-        """
-        This functions is a great helper when you enter the customer's
-        location. It solves the problem of various cities with the same name in
-        a country
-        @param country_id: Country Id (ISO)
-        @param state_id: State Id (ISO)
-        @return: object
-        """
-        if self.country_id:
-            return {'domain': {'state_id': [('country_id', '=', self.country_id.id)]}}
-        else:
-            return {'domain': {'state_id': []}}
-       
-    @api.onchange('state_id')
-    def _onchange_state_id(self):
-        if self.state_id:
-            return {'domain': {'xcity_id': [('state_id', '=', self.state_id.id)]}}
-        else:
-            return {'domain': {'xcity_id': []}}
-
+    
 class SeveralFields(models.Model):
     _description = 'Modelo para Manipular Many2many'
     _name = 'model.manipulate.many2many'
